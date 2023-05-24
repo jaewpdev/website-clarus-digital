d76c307033eb48b379881d0dbcb8ed4ee1fcd968
Jae Park <jae.park@carney.co>
2023-05-24 13:39:49 -0400

    Removes black prop from Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { ReactComponent as ImgClarusDigitalLogo } from '../assets/img--clarus-digital-logo.svg';
 
-export default function Logo({ black }: { black?: boolean }) {
+export default function Logo() {
   return (
     <NavLink to="/" className="flex items-end gap-1">
       <ImgClarusDigitalLogo className="w-8 h-8" />
