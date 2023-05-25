7cb6686a8c84210ae74baa5ba6c602ae26ae24c2
Jae Park <jae.park@carney.co>
2023-05-24 20:30:55 -0400

    Finish steps section.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,12 @@
 import { Helmet } from 'react-helmet';
 
 import { ReactComponent as ImgSparks } from '../assets/img--sparks.svg';
+import ImgIdentification from '../assets/img--identification.png';
+import ImgStrategy from '../assets/img--strategy.png';
+import ImgLead from '../assets/img--lead.png';
+import ImgFollowUp from '../assets/img--followup.png';
+import ImgProfit from '../assets/img--profit.png';
+
 import { Button } from '../components/Button';
 
 function HeroSection() {
@@ -11,7 +17,7 @@ function HeroSection() {
           Uncover Your <br /> Growth Potential
         </h1>
 
-        <p className="max-w-[50ch] xl:max-w-[50ch] 2xl:max-w-[55ch] text-center text-lg md:text-xl my-6 sm:mt-8 sm:mb-12">
+        <p className="max-w-[50ch] 2xl:max-w-[55ch] text-center text-lg md:text-xl my-6 sm:mt-8 sm:mb-12">
           Our goal is simple: to help your business succeed online. We'll
           connect you with your audience and turn those online engagements into
           sales.
@@ -31,6 +37,63 @@ function HeroSection() {
   );
 }
 
+function StepsSection() {
+  const steps = [
+    {
+      title: 'Identification',
+      description:
+        "You have a great business, but reaching the right online audience feels overwhelming, or maybe you don't have an online presence at all.",
+      imgSrc: ImgIdentification,
+    },
+    {
+      title: 'Strategy & Implementation',
+      description:
+        "That's where we come in. We jumpstart your online presence with expertly targeted Google or Facebook ads. We'll manage PPC campaigns and implement strategic retargeting as needed.",
+      imgSrc: ImgStrategy,
+    },
+    {
+      title: 'Lead Capture',
+      description:
+        'We then build a user-friendly and highly effective website funnel. This is your 24/7 net, designed to capture interested leads effortlessly.',
+      imgSrc: ImgLead,
+    },
+    {
+      title: 'Follow-Up & Conversion',
+      description:
+        "We won't stop there. We'll help ensure these leads are followed up with, improving the likelihood of converting them into paying customers.",
+      imgSrc: ImgFollowUp,
+    },
+    {
+      title: 'Profit & Growth',
+      description:
+        "With everything in place, you can enjoy seeing your profits grow. And as your business expands, we'll be there to continuously optimize and improve.",
+      imgSrc: ImgProfit,
+    },
+  ];
+
+  return (
+    <section>
+      <h2 className="text-4xl sm:text-5xl xl:text-6xl font-medium mt-32 mb-12">
+        Your Growth Journey with Us
+      </h2>
+      <p className="max-w-[60ch] text-lg md:text-xl my-6 sm:mt-8 sm:mb-20">
+        Discover how we transform your online presence into a powerful growth
+        engine. Our clear, effective process turns complexity into simplicity,
+        helping you navigate the digital landscape with confidence.
+      </p>
+      <ul className="flex flex-wrap items-center gap-16 justify-center">
+        {steps.map(({ title, description, imgSrc }, index) => (
+          <li key={title} className="w-[400px] min-h-[450px] rounded-lg flex flex-col p-8 shadow-md">
+              <img src={imgSrc} alt={title} className="mb-8 aspect-square w-1/2 mx-auto" />
+              <h6 className="text-lg font-medium mb-2">{index + 1}. {title}</h6>
+              <p className="text-gray-500">{description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
 export default function Home() {
   return (
     <>
@@ -38,6 +101,8 @@ export default function Home() {
         <title>Clarus Digital — Home</title>
       </Helmet>
       <HeroSection />
+      <StepsSection />
+      <div className="h-[1000px]" />
     </>
   );
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -6,7 +6,7 @@ export default function Root() {
   return (
     <div className="font-sans">
       <Header />
-      <div className="container mx-auto my-12">
+      <div className="container mx-auto my-12 overflow-x-hidden">
         <Outlet />
       </div>
     </div>
